68194c5cae6550829f57a9b61a1aad303b2a610a
Notevenmore <hafizsecond635@gmail.com>
2024-10-13 20:36:39 +0700

    Menambahkan kondisi format file yang diizinkan

--- app.py
+++ app.py
@@ -10,14 +10,21 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-@app.route("/read-sgy-segy-file", methods=["POST"])
+@app.route("/read-file", methods=["POST"])
 def sendData():
   if 'file' not in request.files:
     return jsonify({"message": "No Files Accepted!", "status": 400}), 400
   file = request.files.get('file')
+  format = request.form.get('format')
+
   try:
-    [result, data] = convertsegy.convertsegy(file)
-    return jsonify({"result": result, "data": data, "status": 200, "message": "Data has been imported!"}), 200
+    if format == "SGY/SEGY" : 
+      [result, data] = convertsegy.convertsegy(file)
+      return jsonify({"result": result, "data": data, "status": 200, "message": "Data has been imported!"}), 200
+    elif format == "LAS":
+      return jsonify({"status": 500, "message": "Feature coming soon"}), 500
+    else:
+      return jsonify({"status": 400, "message": "Your request file has not accepted!"}), 500
   except Exception as e:
     logger.debug(e)
     return jsonify({"error": str(e), "status": 500, "message": "Data failed to imported!"}), 500
